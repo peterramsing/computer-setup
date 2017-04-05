eb25a23fda5a02aaa0d158524bea063ae126d5f6
Peter Ramsing <peter.ramsing@mac.com>
2017-04-05 08:30:19 -0700

    Adds Ruby gems and some ZSH

--- custom-settings.sh
+++ custom-settings.sh
@@ -17,3 +17,6 @@ defaults write com.apple.finder AppleShowAllFiles YES
 ## Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
+
+## Oh My ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
